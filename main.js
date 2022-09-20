5d68d886e4c21f1506c293ebfc40fda8e6ed1f5a
Khalentello <nicofelipe26@hotmail.com>
2022-09-20 12:58:49 -0500

    Se agrega el menu en mobile y desktop, ademas del aside del carrito de compras

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 var menuEmail = document.querySelector('.navbar-email');
 var desktopMenu = document.querySelector('.desktop-menu');
+var mobileMenuIcon = document.querySelector('.menu');
+var mobileMenu = document.querySelector('.mobile-menu');
+var shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+var shoppingCartList = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+mobileMenuIcon.addEventListener('click',toggleMobileMenu);
+shoppingCartIcon.addEventListener('click',toggleShoppingCartList);
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive')
+    shoppingCartList.classList.add('inactive')
+}
+function toggleMobileMenu (){
+    mobileMenu.classList.toggle('inactive')
+    shoppingCartList.classList.add('inactive')
+}
+function toggleShoppingCartList (){
+    shoppingCartList.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
 }
